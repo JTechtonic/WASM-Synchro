91afef6fb8e19911930727f80e3dfaa41532f931
JTechtonic <jteix200396@gmail.com>
2026-03-10 20:45:19 -0400

    Made a v1 of spinlock; swapping to pc

--- main.c
+++ main.c
@@ -1,29 +1,67 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdatomic.h>
+
+void initAtomics();
+
+#define SPINLOCK_LOCKED 1
+#define SPINLOCK_UNLOCKED 0
+
+_Atomic int* global_spinlock;
+
+#define NUM_THREADS 10 // Adjust --max-threads=N flag to match this
 
 int counter = 0;
 
-#define NUM_THREADS 4 // 4 is max for some reason?
+bool atomic_test_and_set(_Atomic int* spinlock)
+{
+	int free = SPINLOCK_UNLOCKED;
+	int locked = SPINLOCK_LOCKED;
+
+	// Checks to see if spinlock is free; if it is free then make it locked
+	bool obtainedLock = atomic_compare_exchange_strong(spinlock, &free, locked);
+
+	// Returns true if the spinlock used to be free and is now locked; false otherwise
+    return obtainedLock;
+}
+
+void spinlock_lock(_Atomic int* spinlock)
+{
+	while (!atomic_test_and_set(spinlock)) {};
+}
+
+void spinlock_unlock(_Atomic int* spinlock)
+{
+	*spinlock = SPINLOCK_UNLOCKED;
+}
+
 
 void *thread_routine(void *arg)
 {
+	//spinlock_lock(global_spinlock);
 	for (int i = 0; i < 1000000; i++)
 	{
+		spinlock_lock(global_spinlock);
 		counter++;
+		spinlock_unlock(global_spinlock);
 	}
+	//spinlock_unlock(global_spinlock);
 
 	return NULL;
 }
 
-int main(int argc, char** argv)
+int main()
 {
-	pthread_t threads[10];
+	initAtomics();
+
+	pthread_t threads[NUM_THREADS];
 	for (int i = 0; i < NUM_THREADS; i++)
 	{
 		int ret = pthread_create(&threads[i], NULL, thread_routine, NULL);
     	if (ret)
-      		printf("failed to spawn thread: %d", ret);
+      		printf("failed to spawn thread: %d\n", ret);
 
   	}
 
@@ -33,5 +71,12 @@ int main(int argc, char** argv)
   	}
 
 	printf("%d\n", counter);
+	printf("Is supposed to be: %d\n", NUM_THREADS * 1000000);
 	return 0;
+}
+
+void initAtomics()
+{
+	atomic_init(global_spinlock, SPINLOCK_UNLOCKED);
+	//wasm_cluster_set_max_thread_num(100);
 }
